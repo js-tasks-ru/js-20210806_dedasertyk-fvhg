99d78dd8d83c0b77207cc96c80f47a321071d502
Evi-ava <dedasertyk.fvhg@gmail.com>
2021-08-24 13:59:30 +0300

    Сделал NotificationMessage компонент

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
 
+    element = null;
+
+    constructor(message = '', {
+            duration = 2000, 
+            type = 'success' 
+    } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.render();
+    }
+
+    getTemplate() {
+        return  `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+                    <div class="timer"></div>
+                    <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                    </div>
+                </div>`
+    }
+
+    render() {
+        const element = document.createElement('div');
+
+        element.innerHTML = this.getTemplate();
+
+        this.element = element.firstElementChild;
+    }
+
+    show(container = document.body) {
+
+        container.append(this.element);
+
+        setTimeout(() => {
+            this.destroy();
+        }, this.duration);
+    }
+
+    destroy() {
+        this.remove();
+    }
+
+    remove() {
+        this.element.remove();
+    }
 }
